60919b451db3d2244af926f28534748d65da697c
Marylle Roche <marylle.roche.21@usjr.edu.ph>
2023-10-12 23:55:26 +0800

    initial project commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Review from './Review';
+function App() {
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
